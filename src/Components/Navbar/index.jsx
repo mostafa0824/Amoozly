ec3d310f00e623d2bbde0354b24e0a266cdb4527
MOSTAFA <mhrmo75@gmail.com>
2025-09-07 18:57:05 +0330

    Add input for searching courses

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { TbUserDown } from "react-icons/tb";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { ScaleLoader } from "react-spinners";
 
 export default function Navbar() {
+  const [inp,setInp]=useState()
+  const [loading,setLoading]=useState(false)
   const cartQuantity=useSelector(state=>state.cart.items)?.length
   const favoritesQuantity=useSelector(state=>state.favorites.items)?.length
   const navigate=useNavigate()
+
+  const handleSubmit=(e)=>{
+    setLoading(true)
+    e.preventDefault()
+    setInp('')
+    setLoading(false)
+  } 
+
+    if (loading) {
+    return (
+      <div className="flex justify-center items-center m-50">
+        <ScaleLoader color="blue" height={100} width={10} />
+      </div>
+    );
+  }
+
   return (
     <nav className="bg-slate-900 text-white shadow-md">
       <div className="max-w-6xl mx-auto px-4 flex justify-between items-center h-16">
@@ -25,7 +45,19 @@ export default function Navbar() {
           <li><Link to="/courses" className="hover:text-blue-400">دوره‌ها</Link></li>
           <li><Link to="/teachers" className="hover:text-blue-400">مدرس‌ها</Link></li>
         </ul>
-
+        {/* search */}
+        <form onSubmit={handleSubmit} className="flex max-w-md">
+                    <input
+                      type="text"
+                      placeholder="جستجو دوره مورد نظر..."
+                      value={inp}
+                      onChange={(e)=>setInp(e.target.value)}
+                      className="flex-1 border border-gray-300 rounded-r-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button type="submit" onClick={()=>navigate(`/found/${inp.replaceAll('/',' ','-')}`)} className="bg-blue-500 hover:bg-blue-600 text-white px-4 rounded-l-lg cursor-pointer">
+                      <FaSearch />
+                    </button>
+                  </form>
         {/* دکمه ورود/ثبت‌نام */}
         <div className="flex items-center gap-5">
           <Link to="/auth" className="bg-blue-500 px-4 py-2 rounded-md hover:bg-blue-600 text-sm">
@@ -33,12 +65,12 @@ export default function Navbar() {
           </Link>
           {/* cart */}
           <div onClick={()=>navigate('/cart')} className="relative cursor-pointer">
-            <span className="absolute left-6 top-2 bg-red-500 w-4 h-4 text-[12px] rounded-full flex items-center justify-center">{cartQuantity}</span>
+            <span className="absolute left-6 top-2 bg-red-500 w-4 h-4 text-[12px] rounded-full flex items-center justify-center">{new Intl.NumberFormat("fa-IR").format(cartQuantity)}</span>
             <MdOutlineShoppingCart className="text-[35px] hover:text-blue-400"/>
           </div>
           {/* favorites */}
           <div onClick={()=>navigate('/favorites')} className="relative cursor-pointer">
-            <span className="absolute left-6 top-2 bg-red-500 w-4 h-4 text-[12px] rounded-full flex items-center justify-center">{favoritesQuantity}</span>
+            <span className="absolute left-6 top-2 bg-red-500 w-4 h-4 text-[12px] rounded-full flex items-center justify-center">{new Intl.NumberFormat("fa-IR").format(favoritesQuantity)}</span>
             <TbUserDown className="text-[35px] hover:text-blue-400"/>
           </div>
         </div>
