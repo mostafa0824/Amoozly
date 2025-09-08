bf7504b92284fdc76ad01623fb9c5c1dc0bd0a5e
MOSTAFA <mhrmo75@gmail.com>
2025-09-09 00:50:05 +0330

    add user

--- src/store/slices/AuthSlice.js
+++ src/store/slices/AuthSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const getToken=localStorage.getItem("token")|| [];
+const getToken=localStorage.getItem("token");
+const getUser=localStorage.getItem('user');
 const initialState={
-  token:getToken
+  token:getToken,
+  user:getUser,
 }
 
 const AuthSlice=createSlice({
@@ -11,11 +13,15 @@ const AuthSlice=createSlice({
   reducers:{
    login:(state,action)=>{
     state.token=action.payload
+    state.user = action.payload.user;
     localStorage.setItem("token",state.token)
+    localStorage.setItem('user',state.user)
    },
    logout:(state)=>{
     state.token=null
+    state.user=null
     localStorage.removeItem("token")
+    localStorage.removeItem('user')
    }
   }
 })
