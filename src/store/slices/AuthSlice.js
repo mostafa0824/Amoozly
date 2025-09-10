38c006f5e0604cb100b1e9ecdb06b335bc139aaa
MOSTAFA <mhrmo75@gmail.com>
2025-09-10 19:52:30 +0330

    reforms

--- src/store/slices/AuthSlice.js
+++ src/store/slices/AuthSlice.js
@@ -1,30 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const getToken=localStorage.getItem("token");
-const getUser=localStorage.getItem('user');
-const initialState={
-  token:getToken,
-  user:getUser,
-}
+const getToken = localStorage.getItem("token");
+const getUser = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
 
-const AuthSlice=createSlice({
-  name:"AuthSlice",
+const initialState = {
+  token: getToken,
+  user: getUser,
+};
+
+const AuthSlice = createSlice({
+  name: "AuthSlice",
   initialState,
-  reducers:{
-   login:(state,action)=>{
-    state.token=action.payload
-    state.user = action.payload.user;
-    localStorage.setItem("token",state.token)
-    localStorage.setItem('user',state.user)
-   },
-   logout:(state)=>{
-    state.token=null
-    state.user=null
-    localStorage.removeItem("token")
-    localStorage.removeItem('user')
-   }
-  }
-})
+  reducers: {
+    login: (state, action) => {
+      state.token = action.payload.token;
+      state.user = action.payload.user;
 
-export const{login,logout}=AuthSlice.actions
-export default AuthSlice.reducer
+      localStorage.setItem("token", state.token);
+      localStorage.setItem("user", JSON.stringify(state.user));
+    },
+    logout: (state) => {
+      state.token = null;
+      state.user = null;
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+    },
+  },
+});
+
+export const { login, logout } = AuthSlice.actions;
+export default AuthSlice.reducer;
