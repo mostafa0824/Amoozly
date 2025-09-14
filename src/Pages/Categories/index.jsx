e4c06e294405e749b896da804381e6442d5fa393
MOSTAFA <mhrmo75@gmail.com>
2025-09-14 20:25:32 +0330

    add icon

--- src/Pages/Categories/index.jsx
+++ src/Pages/Categories/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import CardCategories from './cardCategories';
 import fetchData from '../../Utils/fetchData';
 import { ScaleLoader } from 'react-spinners';
+import { BiCategory } from 'react-icons/bi';
 
 export default function Categories() {
   const [loading,setLoading]=useState(true)
@@ -27,6 +28,10 @@ export default function Categories() {
 
   return (
     <div className='container mx-auto px-5 sm:px-5 py-6'>
+      <h1 className="text-2xl font-bold text-gray-900 mb-6 flex items-center">
+                <BiCategory className="ml-2 text-[35px] text-blue-600" />
+                 دسته بندی دوره های آموزشی
+              </h1>
       <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-4">
           {itemCtegory}
       </div>
